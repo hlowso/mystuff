cce03daa72e608043865d5355bee0aa1b403c8b1
hlowso <h.canderleigh@gmail.com>
2018-02-15 23:30:42 -0500

    removed everything

--- mytheory/bass/bassist.py
+++
@@ -1,9 +0,0 @@
-import mystuff.mytheory.hard_coded
-import mystuff.mytheory.general
-import mystuff.mytheory.jazz 
-import mystuff.mytheory.bass.walk 
-
-class Bassist(mystuff.mytheory.jazz.Comper):
-	def __init__(self, bass):
-		mystuff.mytheory.jazz.Comper.__init__(self, bass)
-		self.comp_styles['swung'] = mystuff.mytheory.bass.walk.walk
--- mytheory/kit/kit.py
+++
@@ -1,4 +0,0 @@
-class Drummer(mystuff.mytheory.jazz.Comper):
-	def __init__(self, kit):
-		mystuff.mytheory.jazz.Comper.__init__(self, kit)
-		self.comp_styles['swung'] = mystuff.mytheory.kit.swung_groove.groove()
